a92880c3bc04de90512c4bb6f57badbabaca427c
navyxie2010@gmail.com <951178609.com>
2015-10-29 00:36:59 +0800

    add angular test demo

---
+++ js/test/filter.js
@@ -0,0 +1,13 @@
+describe('filter', function() {
+  beforeEach(module('myApp'));
+
+  describe('interpolate', function() {
+    beforeEach(module(function($provide) {
+      $provide.value('version', 'TEST_VER');
+    }));
+
+    it('should replace VERSION', inject(function(interpolateFilter) {
+      expect(interpolateFilter('before %VERSION% after')).toEqual('before TEST_VER after');
+    }));
+  });
+});
